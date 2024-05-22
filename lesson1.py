287e467237f5ea933f1d65176e7f996eeeaa4c75
Damini Karfa <86921150+daminikarfa@users.noreply.github.com>
2024-05-22 15:40:54 +0530

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,10 @@
+#we can use both single and double quotation in Python
+print('Hello World!')
+#The p in print must be in lowercase because python is a case sensitive language
+print("It's DAY-1")
+#we have to be careful about quotations to avoid syntax errors 
+print("I am printing 'Strings'.")
+#sting concatenation
+print("Iron" + "man")
+print("Iron"+" "+"man")
+
