e28092d75c6699c560118ed692df3e4b74f63403
Damini Karfa <86921150+daminikarfa@users.noreply.github.com>
2024-05-25 00:01:40 +0530

    Add files via upload

---
+++ lesson4.py
@@ -0,0 +1,12 @@
+# There are two variables, a and b from input
+a = input("write a value for a:")
+b = input("write a value for b:")
+# 🚨 Don't change the code above ☝️
+####################################
+# Write your code below this line 👇
+c=a
+a=b
+b=c
+# Write your code above this line ☝️
+print("a: " + a)
+print("b: " + b)
